36fbb064e36525d3965626d4b9a3bcd2b0960874
li16za <li162002@mail.ru>
2022-06-21 13:16:42 +0300

    correct auth

--- src/application/AuthController.java
+++ src/application/AuthController.java
@@ -1,6 +1,10 @@
 package application;
 
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -27,9 +31,21 @@ public class AuthController {
 
     @FXML
     void SingIn(ActionEvent event) {
-
- 
-    }	
+        ArrayList<Sotrudniki> sotrudniki = new ArrayList<Sotrudniki>();
+        System.out.println(login);
+        System.out.println(password);
+        Connect connects = new Connect();
+        try (Connection connection = connects.getConnection();
+                Statement statement = connection.createStatement();) {
+            String selectSql = "SELECT * from sotrudniki";
+            ResultSet result = statement.executeQuery(selectSql);
+            while (result.next()){
+                sotrudniki.add(new Sotrudniki( result.getInt(1), result.getString(2),result.getInt(3), result.getString(4),result.getString(5), last_sing_in, type_sign_in));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
     @FXML
     void initialize() {
