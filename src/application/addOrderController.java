b10e36c846be64d3e02b779f63e84f37a2c43dc8
li16za <li162002@mail.ru>
2022-06-21 23:42:40 +0300

    add function sellman

---
+++ src/application/addOrderController.java
@@ -0,0 +1,142 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class addOrderController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button AddOrder;
+
+    @FXML
+    private TextField code;
+
+    @FXML
+    private TextField dateClose;
+
+    @FXML
+    private TextField dateCreate;
+
+    @FXML
+    private TextField id;
+
+    @FXML
+    private TextField idClient;
+
+    @FXML
+    private Button singOut;
+
+    @FXML
+    private TextField status;
+
+    @FXML
+    private TextField timeOrder;
+
+    @FXML
+    private TextField timeProkata;
+
+    @FXML
+    void AddOrder(ActionEvent event) {
+    	if (id.getText().isEmpty() || code.getText().isEmpty() || dateCreate.getText().isEmpty() || timeOrder.getText().isEmpty() 
+    	|| idClient.getText().isEmpty() || status.getText().isEmpty() || dateClose.getText().isEmpty() || timeProkata.getText().isEmpty() )	
+    	{
+    		Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Ошибка");
+			alert.setHeaderText("Невозможно оформить заказ");
+			alert.setContentText("Проверьте правильность заполнения полей");
+			alert.showAndWait();
+    	}
+    	else {	
+    	String insertQuery = "INSERT INTO prokat.order "
+    			+ "	(id,\n"
+    			+ "    	code,\n"
+    			+ "    	cretion_date,\n"
+    			+ "    	order_time,\n"
+    			+ "    	id_client,\n"
+    			+ "    	status,\n"
+    			+ "    	close_date,\n"
+    			+ "    	time_prokat)" +
+                "values (\'" +
+                id.getText()+ "\', \'" +
+                code.getText()+ "\', \'" +
+                dateCreate.getText()+ "\', \'" +
+                timeOrder.getText()+ "\', \'" +
+                idClient.getText() + "\', \'" +
+                status.getText()+ "\', \'" +
+                dateClose.getText()+ "\', \'" +
+                timeProkata.getText()+ "\');";
+        TransactionController.MakeTransaction(insertQuery);
+        Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+    	AnchorPane root;
+		try {
+			root = (AnchorPane) FXMLLoader.load(getClass().getResource("menuSellman.fxml"));
+			Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.setTitle("точка проката ЦПКиО им. С.М. Кирова");
+	        stage.setResizable(true);
+	        stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setTitle("Ошибка");
+			alert.setHeaderText("Невозможно оформить заказ");
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		}
+		}
+    }
+
+    @FXML
+    void SingOut(ActionEvent event) {
+    	Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+    	AnchorPane root;
+		try {
+			root = (AnchorPane) FXMLLoader.load(getClass().getResource("auth.fxml"));
+			Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.setTitle("точка проката ЦПКиО им. С.М. Кирова");
+	        stage.setResizable(true);
+	        stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void initialize() {
+        assert AddOrder != null : "fx:id=\"AddOrder\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert code != null : "fx:id=\"code\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert dateClose != null : "fx:id=\"dateClose\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert dateCreate != null : "fx:id=\"dateCreate\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert id != null : "fx:id=\"id\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert idClient != null : "fx:id=\"idClient\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert singOut != null : "fx:id=\"singOut\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert status != null : "fx:id=\"status\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert timeOrder != null : "fx:id=\"timeOrder\" was not injected: check your FXML file 'addOrder.fxml'.";
+        assert timeProkata != null : "fx:id=\"timeProkata\" was not injected: check your FXML file 'addOrder.fxml'.";
+
+    }
+
+}
---
+++ src/application/menuSellmanController.java
@@ -0,0 +1,157 @@
+package application;
+
+import java.io.IOException;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Date;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class menuSellmanController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button AddOrder;
+    
+    @FXML
+    private TableView<Order> Order;
+    
+    @FXML
+    private TableColumn<Order, String> code;
+
+    @FXML
+    private TableColumn<Order, Date> dateClose;
+
+    @FXML
+    private TableColumn<Order, Date> dateCreate;
+
+    @FXML
+    private TableColumn<Order, Integer> id;
+
+    @FXML
+    private TableColumn<Order, Integer> idClient;
+
+    @FXML
+    private Button singOut;
+
+    @FXML
+    private TableColumn<Order, Integer> status;
+
+    @FXML
+    private TableColumn<Order, Date> timeOrder;
+
+    @FXML
+    private TableColumn<Order, Integer> timeProkata;
+
+    @FXML
+    void AddOrder(ActionEvent event) {
+    	Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+    	AnchorPane root;
+		try {
+			root = (AnchorPane) FXMLLoader.load(getClass().getResource("addOrder.fxml"));
+			Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.setTitle("точка проката ЦПКиО им. С.М. Кирова");
+	        stage.setResizable(true);
+	        stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Alert alert = new Alert(AlertType.ERROR);
+	    	alert.setTitle("Ошибка");
+			alert.setHeaderText("Невозможно оформить заказ");
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		}
+    }
+
+    @FXML
+    void SingOut(ActionEvent event) {
+    	Node source = (Node) event.getSource();
+        Stage stage = (Stage) source.getScene().getWindow();
+    	AnchorPane root;
+		try {
+			root = (AnchorPane) FXMLLoader.load(getClass().getResource("auth.fxml"));
+			Scene scene = new Scene(root);
+	        stage.setScene(scene);
+	        stage.setTitle("точка проката ЦПКиО им. С.М. Кирова");
+	        stage.setResizable(true);
+	        stage.show();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Alert alert = new Alert(AlertType.ERROR);
+	    	alert.setTitle("Ошибка");
+			alert.setHeaderText("Невозможно завершить сессию");
+			alert.setContentText(e.getMessage());
+			alert.showAndWait();
+		}
+    }
+
+    @FXML
+    void initialize() {
+    	Connect connects = new Connect();
+    	ObservableList<Order> orders = FXCollections.observableArrayList();
+        try (Connection connection1 = connects.getConnection();
+             Statement statement = connection1.createStatement();) {
+            String selectSql = "SELECT * from prokat.order;";
+            ResultSet result = statement.executeQuery(selectSql);
+            while (result.next()) {
+            	orders.add(new Order(result.getInt(1), result.getString(2), result.getDate(3), result.getDate(4), result.getInt(5), result.getInt(6),result.getDate(7),result.getInt(8)));
+            }
+          //There's a bug somewhere, I don't know where
+            id.setCellValueFactory(new PropertyValueFactory<Order, Integer>("id"));
+            code.setCellValueFactory(new PropertyValueFactory<Order, String>("code"));
+            timeOrder.setCellValueFactory(new PropertyValueFactory<Order, Date>("orderTime"));
+            dateCreate.setCellValueFactory(new PropertyValueFactory<Order, Date>("cretionDate"));
+            idClient.setCellValueFactory(new PropertyValueFactory<Order, Integer>("idClient"));
+            status.setCellValueFactory(new PropertyValueFactory<Order, Integer>("status"));
+            dateClose.setCellValueFactory(new PropertyValueFactory<Order, Date>("closeDate"));
+            timeProkata.setCellValueFactory(new PropertyValueFactory<Order, Integer>("timeProkat"));
+            Order.setItems(orders);
+    } catch (SQLException e) {
+    	e.printStackTrace();
+		Alert alert = new Alert(AlertType.ERROR);
+    	alert.setTitle("Ошибка");
+		alert.setHeaderText("Невозможно отобразить заказы");
+		alert.setContentText(e.getMessage());
+		alert.showAndWait();
+        }
+        assert AddOrder != null : "fx:id=\"AddOrder\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert code != null : "fx:id=\"code\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert dateClose != null : "fx:id=\"dateClose\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert dateCreate != null : "fx:id=\"dateCreate\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert id != null : "fx:id=\"id\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert idClient != null : "fx:id=\"idClient\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert singOut != null : "fx:id=\"singOut\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert status != null : "fx:id=\"status\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert timeOrder != null : "fx:id=\"timeOrder\" was not injected: check your FXML file 'menuSellman.fxml'.";
+        assert timeProkata != null : "fx:id=\"timeProkata\" was not injected: check your FXML file 'menuSellman.fxml'.";
+
+    }
+
+}
